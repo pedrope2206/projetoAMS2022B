08e228ae0efb8d1f425300a1e566d0c3be0a1ed2
pedrope2206 <90793651+pedrope2206@users.noreply.github.com>
2022-10-06 09:01:38 -0300

    Add files via upload

---
+++ ex_numeros_triangulares/Program.cs
@@ -0,0 +1,25 @@
+﻿int nt = 0;
+int contador = 0;
+int nd = 0;
+
+Console.WriteLine("Digite um número");
+nd = Convert.ToInt32(Console.ReadLine());
+
+//contador++;
+
+
+while (nt < nd)
+{
+    contador++;
+    nt = (contador + nt );
+}
+
+if (nd == nt)
+{
+    Console.WriteLine("Este número é triângular");
+}
+else
+{
+    Console.WriteLine("Este número não é triângular");
+}
+
